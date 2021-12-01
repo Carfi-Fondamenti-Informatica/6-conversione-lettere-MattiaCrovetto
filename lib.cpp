530a1b19a1e05e3b563163076f58ba7f4b645b0a
MattiaCrovetto <92729247+MattiaCrovetto@users.noreply.github.com>
2021-12-01 17:47:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,8 @@
 #include "lib.h"
-
+#include <iostream>
 bool verifica(char a){
- if((a>=65 && a<=90)||(a>=97 && a<=122)){
-     return true;
- }else{
-     return false;
- }
-}
-char convert(char a){
-    if(a>=65 && a<=90){
-        a=a+32;
-        return a;
-    }else if(a>=97 && a<=122){
-        a=a-32;
-        return a;
+    if((a>=97&&a<=122)||(a>=65&&a<=90)){
+        return true;
     }
+    return false;
+}
