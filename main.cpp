69c357c7b7e626a07358b2397aa4e38f8fd4187d
MattiaCrovetto <92729247+MattiaCrovetto@users.noreply.github.com>
2021-12-01 17:42:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char a;
+  cin>>a;
+  int ris= verifica(a);
+  
+  if(ris){
+    cout << convert(a) << endl;
+  }
+  else{
+    cout << "errore";
+  }
   return 0;
 }
